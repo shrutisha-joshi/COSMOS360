f29e5c99d0bff23a003a452790c8c87a22f2a165
Shrutisha <shrutisha10sep98@gmail.com>
2018-06-30 16:16:54 +0530

    made qr code efficient

--- app/src/main/java/com/robovitics/cosmos360/cosmos_360/ProfileFragment.java
+++ app/src/main/java/com/robovitics/cosmos360/cosmos_360/ProfileFragment.java
@@ -13,6 +13,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
 /**
@@ -49,29 +50,11 @@ public class ProfileFragment extends Fragment {
     }
 
     private Bitmap TextToImageEncode(String Value) throws WriterException {
-        BitMatrix bitMatrix;
-        try{
-            bitMatrix = new MultiFormatWriter().encode(Value, BarcodeFormat.DATA_MATRIX.QR_CODE,QRcodeWidth,QRcodeWidth,null);
-        } catch (IllegalArgumentException Ie){
-            return null;
-        }
-
-        int bitMatrixWidth = bitMatrix.getWidth();
-        int bitMatrixHeight = bitMatrix.getHeight();
-        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
-
-        for (int y = 0;y<bitMatrixHeight; y++){
-            int offset = y * bitMatrixWidth;
-
-            for (int x=0; x<bitMatrixWidth;x++){
-                pixels[offset+x]= bitMatrix.get(x,y)? getResources().getColor(R.color.colorBlack):getResources().getColor(R.color.colorWhite);
-            }
-
-        }
-        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
-
-        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
-        return bitmap;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        BitMatrix bitMatrix = multiFormatWriter.encode(Value, BarcodeFormat.QR_CODE,500,500);
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+        Bitmap bitmpap = barcodeEncoder.createBitmap(bitMatrix);
+        return bitmpap;
     }
 
 }
